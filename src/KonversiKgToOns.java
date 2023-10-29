3b2f7e96ab66644388b44cba8e9966931964d05a
AryaMandala33 <aryamp33@gmail.com>
2023-10-29 21:33:19 +0700

    upload modul 3

---
+++ src/KonversiKgToOns.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class KonversiKgToOns {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        /**
+         * Untuk meng input berat dalam kilogram
+         */
+
+        System.out.print("Masukkan berat dalam kilogram (KG): ");
+        double beratKg = input.nextDouble();
+
+        /**
+         * rumus konversi dari berat KG to ons
+         */
+        double ons = beratKg * 35.274; // 1 KG = 35.274 Ounce
+
+        System.out.println(beratKg + " kilogram adalah setara dengan " + ons + " ons.");
+    }
+}}
